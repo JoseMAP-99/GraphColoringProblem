f86eeeb2d83dbc59ade36972db1b5689fdc9a0d8
JoseMAP-99 <55493484+JoseMAP-99@users.noreply.github.com>
2020-04-04 00:53:36 +0100

    Update Grafos.py

--- Grafos.py
+++ Grafos.py
@@ -1,12 +1,23 @@
 # Input data files are available in the "../input/" directory.
 # For example, running this (by clicking run or pressing Shift+Enter) will list all files under the input directory
-#import numpy as np  # linear algebra
-#import pandas as pd  # data processing, CSV file I/O (e.g. pd.read_csv)
+# import numpy as np  # linear algebra
+# import pandas as pd  # data processing, CSV file I/O (e.g. pd.read_csv)
 import os
-import minizinc
+from minizinc import Instance, Model, Solver
+import csv
+import collections
+from IPython.display import FileLink
 
 # Ahí hay ejemplos de uso de minizinc-Python
 # https://minizinc-python.readthedocs.io/en/latest/
+
+
+def submission_generation(filename, str_output):
+    with open(filename, 'w', newline='') as file:
+        writer = csv.writer(file)
+        for item in str_output:
+            writer.writerow(item)
+    return FileLink(filename)
 
 
 def check_solution(node_count, edges, solution):
@@ -17,6 +28,37 @@ def check_solution(node_count, edges, solution):
     value = max(solution) + 1  # add one because minimum color is 0
 
     return value
+
+
+def initializeMinizinc(inicios, finales, edge_count, node_count):
+    # Create a MiniZinc model
+    model = Model()
+
+    model.add_string("""
+            int: e;
+            int: n;
+            set of int: nodes;
+            array[1..e] of int: Inicio;
+            array[1..e] of int: Final;
+            array[0..n] of var nodes: Des;
+            
+            constraint forall(i in 1..e)(Des[Inicio[i]] != Des[Final[i]]);
+            
+            solve satisfy;
+            """)
+
+    # Transform Model into a instance
+    gecode = Solver.lookup("gecode")
+    inst = Instance(gecode, model)
+    inst["e"] = edge_count
+    inst["n"] = node_count-1
+    inst["nodes"] = range(0, node_count-1)
+    inst["Inicio"] = inicios
+    inst["Final"] = finales
+
+    # Solve the instance
+    result = inst.solve()
+    return result["Des"]
 
 
 def solve_it(input_data):
@@ -30,37 +72,19 @@ def solve_it(input_data):
     edge_count = int(first_line[1])
 
     edges = []
+    inicio = []
+    final = []
     for i in range(1, edge_count + 1):
         line = lines[i]
         parts = line.split()
         edges.append((int(parts[0]), int(parts[1])))
-    
-    
-    # Create a MiniZinc model
-    model = minizinc.Model()
-    model.add_string("""
-    var -100..100: x;
-    int: a; int: b; int: c;
-    constraint a*(x*x) + b*x = c;
-    solve satisfy;
-    """)
+        inicio.append(int(parts[0]))
+        final.append(int(parts[1]))
 
-    # Transform Model into a instance
-    gecode = minizinc.Solver.lookup("gecode")
-    inst = minizinc.Instance(gecode, model)
-    inst["a"] = 1
-    inst["b"] = 0
-    inst["c"] = 4
+    solution = initializeMinizinc(inicio, final, edge_count, node_count)
 
-    # Solve the instance
-    result = inst.solve(all_solutions=True)
-    for i in range(len(result)):
-        print("x = {}".format(result[i, "x"]))
-
-        
     # build a trivial solution
     # every node has its own color
-    solution = range(0, node_count)
 
     # prepare the solution in the specified output format
     output_data = str(node_count) + ' ' + str(0) + '\n'
@@ -84,3 +108,5 @@ if __name__ == "__main__":
                 output, value = solve_it(input_data)
                 print(filename, value)
                 str_output.append([filename, str(value)])
+
+    submission_generation('Grupo1_Provisional.csv', str_output)
