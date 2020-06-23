d9315e8047e4c82a6241a50761b6411b18142c00
Prashant-JT <55165517+Prashant-JT@users.noreply.github.com>
2020-06-23 03:16:47 +0100

    using two different models depending on the number of edges

--- graphColoring.py
+++ graphColoring.py
@@ -1,14 +1,10 @@
 # import numpy as np  # linear algebra
 # import pandas as pd  # data processing, CSV file I/O (e.g. pd.read_csv)
+import collections
 import os
 from minizinc import Instance, Model, Solver
 import csv
 from IPython.display import FileLink
-
-
-# Ahí hay ejemplos de uso de minizinc-Python
-# https://minizinc-python.readthedocs.io/en/latest/
-
 
 def submission_generation(filename, str_output):
     with open(filename, 'w', newline='') as file:
@@ -28,22 +24,57 @@ def check_solution(node_count, edges, solution):
     return value
 
 
-def initializeMinizinc(inicios, finales, edge_count, node_count):
+def initializeMinizinc(maximo, edges, edge_count, node_count):
     # Create a MiniZinc model
     model = Model()
 
     model.add_string("""
             include "globals.mzn";
+            int: e;
+            int: n;
+            int: m;
+            set of int: nodes = 0..m;
+            array[1..e, 1..2] of int: edges;
+            array[0..n] of var nodes: Des;
+                            
+            var nodes: colors;
+                            
+            constraint Des[0] == 0;
+            constraint forall(i in 1..e)(Des[edges[i,1]] != Des[edges[i,2]]);
+            constraint forall(x, y in 0..n where y = x+1)(value_precede(x, y, Des) /\ 
+            Des[y] <= colors);
             
+            solve minimize colors;
+            """)
+
+    # Transform Model into a instance
+    gecode = Solver.lookup("gecode")
+    inst = Instance(gecode, model)
+    inst["e"] = edge_count
+    inst["n"] = node_count
+    inst["m"] = maximo
+    inst["edges"] = edges
+
+    # Solve the instance
+    result = inst.solve()
+    return result["Des"]
+
+
+def initializeMinizinc2(edges, edge_count, node_count):
+    # Create a MiniZinc model
+    model = Model()
+
+    model.add_string("""
+            include "globals.mzn";
+
             int: n;
             int: e;
             set of int: nodes;
-            array[1..e] of int: Inicio;
-            array[1..e] of int: Final;
+            array[1..e, 1..2] of int: edges;
             array[0..n] of var nodes: Des;
-
-            constraint forall(i in 1..e)(Des[Inicio[i]] != Des[Final[i]]);
             
+            constraint forall(i in 1..e)(Des[edges[i,1]] != Des[edges[i,2]]);
+
             solve :: int_search(Des, most_constrained, indomain_min) satisfy;
             """)
 
@@ -51,10 +82,9 @@ def initializeMinizinc(inicios, finales, edge_count, node_count):
     gecode = Solver.lookup("gecode")
     inst = Instance(gecode, model)
     inst["e"] = edge_count
-    inst["n"] = node_count - 1
-    inst["nodes"] = range(0, node_count - 1)
-    inst["Inicio"] = inicios
-    inst["Final"] = finales
+    inst["n"] = node_count
+    inst["nodes"] = range(0, node_count)
+    inst["edges"] = edges
 
     # Solve the instance
     result = inst.solve()
@@ -62,8 +92,6 @@ def initializeMinizinc(inicios, finales, edge_count, node_count):
 
 
 def solve_it(input_data):
-    # Modify this code to run your optimization algorithm
-
     # parse the input
     lines = input_data.split('\n')
 
@@ -72,19 +100,17 @@ def solve_it(input_data):
     edge_count = int(first_line[1])
 
     edges = []
-    inicio = []
-    final = []
     for i in range(1, edge_count + 1):
         line = lines[i]
         parts = line.split()
         edges.append((int(parts[0]), int(parts[1])))
-        inicio.append(int(parts[0]))
-        final.append(int(parts[1]))
 
-    solution = initializeMinizinc(inicio, final, edge_count, node_count)
-
-    # build a trivial solution
-    # every node has its own color
+    if edge_count <= 1200:
+        freq = collections.Counter(t[0] for t in edges)
+        maximo = max(freq.values()) + 1
+        solution = initializeMinizinc(maximo, edges, edge_count, node_count - 1)
+    else:
+        solution = initializeMinizinc2(edges, edge_count, node_count - 1)
 
     # prepare the solution in the specified output format
     output_data = str(node_count) + ' ' + str(0) + '\n'
@@ -109,4 +135,4 @@ if __name__ == "__main__":
                 print(filename, value)
                 str_output.append([filename, str(value)])
 
-    submission_generation('Grupo1_Provisional.csv', str_output)
+    submission_generation('Grupo1_ColoreadoDeGrafos.csv', str_output)
